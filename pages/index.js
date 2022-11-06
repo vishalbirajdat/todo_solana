39ef12c2c29b1bdd53ca291adce6e61705187110
vishal Birajdar <65212461+vishalbirajdat@users.noreply.github.com>
2022-11-06 20:14:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,11 @@ import { useTodo } from '../hooks/todo'
 import Loading from '../components/Loading'
 import TodoSection from '../components/todo/TodoSection'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react/cjs/react.production.min'
 
 
 const Home = () => {
     const { initialized, initializeUser, loading, transactionPending, completedTodos, incompleteTodos, addTodo, markTodo, removeTodo, markStaticTodo,removeStaticTodo, addStaticTodo, } = useTodo()
-    const [Inputs, setInputs] = useState(null)
-    const handleChange = (e)=>{
-         e.preventDefault();
-        console.log(e.target.value);
-        setInputs(e.target.value);
-    }
 
-    const addTodoAdd = (e)=>{
-        e.preventDefault();
-        addTodo(Inputs);
-    }
 
     return (
         <div className={styles.container}>
@@ -27,7 +16,7 @@ const Home = () => {
                     <div className={styles.todoInput}>
                         <div className={`${styles.todoCheckbox} ${styles.checked}`} />
                         <div className={styles.inputContainer}>
-                            <form onSubmit={addTodoAdd}>
+                            <form onSubmit={addTodo}>
                                 <input  onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />
                             </form>
                         </div>
